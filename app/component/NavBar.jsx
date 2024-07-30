8203e6a692e9cb8dd7b5270d485aaa63d573d1f9
PHAM NGUYEN MINH HIEU <118499264+Cierale@users.noreply.github.com>
2024-07-29 22:07:12 -0600

    Update NavBar.jsx

--- app/component/NavBar.jsx
+++ app/component/NavBar.jsx
@@ -1,21 +1,31 @@
-import Link from 'next/link';
+'use client'
 
-const NavBar = () => { // Navbar component for website
-    return (
-        <nav class="bg-red-500 text-blue text-xl p-4 flex justify-between items-center">
-            <div className='font-bold text-black'>REMS employee management app</div>
-            <div>
-                    <Link button class="bg-white text-blue-500 hover:bg-blue-100 text-sm font-semibold py-2 px-4 mr-2 rounded" href="/">
-                        Home
-                    </Link>
-                
-                    <Link button class="bg-white text-blue-500 hover:bg-blue-100 text-sm font-semibold py-2 px-4 mr-2 rounded" href="/admin">
-                        Login
-                    </Link>
-            </div>
-                
-        </nav>
-    );
+import { useRouter } from 'next/navigation'; // Import useRouter from next/navigation
+
+const NavBar = () => {
+  const { push } = useRouter(); // Destructure push method from useRouter
+
+  return (
+    <nav className='bg-red-500 text-blue text-xl p-4 flex justify-between items-center'>
+      <div className='font-bold text-black'>
+        <label>REMS employee management app</label>
+      </div>
+      <div>
+        <button
+          onClick={() => push('/')}
+          className="bg-white text-blue-500 hover:bg-blue-100 text-sm font-semibold py-2 px-4 mr-2 rounded"
+        >
+          Home
+        </button>
+        <button
+          onClick={() => push('/login')}
+          className="bg-white text-blue-500 hover:bg-blue-100 text-sm font-semibold py-2 px-4 mr-2 rounded"
+        >
+          Login
+        </button>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;

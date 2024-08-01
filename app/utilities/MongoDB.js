c521cadc81da2aaacec128ec34d797b21a77bcbf
PHAM NGUYEN MINH HIEU <118499264+Cierale@users.noreply.github.com>
2024-08-01 10:29:37 -0600

    Create MongoDB.js

---
+++ app/utilities/MongoDB.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const connectMongoDB = async () => {
+  if (mongoose.connections[0].readyState) return;
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB', error);
+  }
+};
+
+export { connectMongoDB };
